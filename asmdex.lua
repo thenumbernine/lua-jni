3c3afff25b45fcd14b2bd740e8cd34b404c59735
Christopher Moore <christopher.e.moore@gmail.com>
2026-03-02 21:15:53 +0700

    more

--- asmdex.lua
+++ asmdex.lua
@@ -1,5 +1,6 @@
 local ffi = require 'ffi'
 local class = require 'ext.class'
+local assert = require 'ext.assert'
 local string = require 'ext.string'
 local ReadBlob = require 'java.blob'.ReadBlob
 local WriteBlob = require 'java.blob'.WriteBlob
@@ -56,28 +57,47 @@ end
 function JavaASMDex:readData(data)
 	local blob = ReadBlob(data)
 	blob.littleEndian = true	-- by default
-	assert.eq(blob:readString(8), 'dex\n035\0')
+	assert.eq(blob:readString(4), 'dex\n')
+	local version = blob:readString(4)	-- 3 text chars of numbers with null term ...
+print('version', string.hex(version))
 	local checksum = blob:readu4()
+print('checksum', bit.tohex(checksum, 8))
 	local sha1sig = blob:readString(20)
-	local size = blob:readu4()
+
+print('sha1sig', string.hex(sha1sig))
+	local filesize = blob:readu4()
+print('filesize', bit.tohex(filesize, 8))
+	local headersize = blob:readu4()
+print('headersize', bit.tohex(headersize, 8))
 	local endian = blob:readu4()
+print('endian', bit.tohex(endian, 8))
 	if endian == 0x78563412 then
 		-- then do I flip size and checksum as well?
 		blob.littleEndian = false
+	elseif endian == 0x12345678 then
+		-- safe
+	else
+		io.stderr:write('!!! WARNING !!! endian is a bad value: 0x'..bit.tohex(endian, 8)..', something else will probably go wrong.\n')
 	end
-	assert.eq(size, #data, "size didn't match")	-- when does size not equal #data?
+	assert.eq(filesize, #data, "filesize didn't match")	-- when does size not equal #data?
+
 	local linkSize = blob:readu4()
 	local linkOfs = blob:readu4()
+print('link size', bit.tohex(linkSize, 8),'ofs', bit.tohex(linkOfs, 8))
+
 	local mapOfs = blob:readu4()
+print('map ofs', bit.tohex(mapOfs, 8))
 
 	-- string offset points to a list of uint32_t's which point to the string data
 	-- ... which start with a uleb128 prefix
-	local stringIdsSize = blob:readu4()
-	local stringIdsOfs = blob:readu4()
+	local stringIdSize = blob:readu4()
+	local stringIdOfs = blob:readu4()
+print('stringId size', bit.tohex(stringIdSize, 8),'ofs', bit.tohex(stringIdOfs, 8))
 
 	-- list of uint32 idss into string table?
-	local typeIdsSize = blob:readu4()
-	local typeIdsOfs = blob:readu4()
+	local typeIdSize = blob:readu4()
+	local typeIdOfs = blob:readu4()
+print('typeId size', bit.tohex(typeIdSize, 8),'ofs', bit.tohex(typeIdOfs, 8))
 
 	--[[
 	proto points to:
@@ -85,26 +105,33 @@ function JavaASMDex:readData(data)
 		uint32_t type-ID index of return type
 		uint32_t offset into "type list" (where?)
 	--]]
-	local protoIdsSize = blob:readu4()
-	local protoIdsOfs = blob:readu4()
+	local protoIdSize = blob:readu4()
+	local protoIdOfs = blob:readu4()
+print('protoId size', bit.tohex(protoIdSize, 8),'ofs', bit.tohex(protoIdOfs, 8))
 
 	--[[
 
 	--]]
-	local fieldIdsSize = blob:readu4()
-	local fieldIdsOfs = blob:readu4()
+	local fieldIdSize = blob:readu4()
+	local fieldIdOfs = blob:readu4()
+print('fieldId size', bit.tohex(fieldIdSize, 8),'ofs', bit.tohex(fieldIdOfs, 8))
 
 	--[[
 		uint16 index into type IDs of class
 		uint16 index into protoIDs for method signature
 		uint32 index into stringIDs for method name
 	--]]
-	local methodIdsSize = blob:readu4()
-	local methodIdsOfs = blob:readu4()
+	local methodIdSize = blob:readu4()
+	local methodIdOfs = blob:readu4()
+print('methodId size', bit.tohex(methodIdSize, 8),'ofs', bit.tohex(methodIdOfs, 8))
 
 	local classDefSize = blob:readu4()
 	local classDefOfs = blob:readu4()
+print('classDef size', bit.tohex(classDefSize, 8),'ofs', bit.tohex(classDefOfs, 8))
 
 	local dataSize = blob:readu4()
 	local dataOfs = blob:readu4()
+print('data size', bit.tohex(dataSize, 8),'ofs', bit.tohex(dataOfs, 8))
 end
+
+return JavaASMDex
---
+++ tests/luaclass/test2.lua
@@ -0,0 +1,51 @@
+#!/usr/bin/env luajit
+-- test classes made at runtime
+local table = require 'ext.table'
+local tolua = require 'ext.tolua'
+local J = require 'java'
+
+local JavaLuaClass = require 'java.luaclass'
+local TestClass = JavaLuaClass{
+	env = J,
+	name = 'TestClass',
+	fields = {
+		{
+			name = 'foo',
+			sig = 'double',
+		}
+	},
+	methods = {
+		{
+			name = 'bar',
+			value = function(...)
+				print("JavaLuaClass TestClass bar() says hello!")
+				print('lua got #args', select('#', ...))
+				print('lua got args', ...)
+
+				return 3.14
+				--return J.Double(3.14)
+			end,
+			sig = {
+				'double',
+				--'java.lang.Double',
+				'java.lang.Object',
+			},
+		},
+	},
+}
+
+print('TestClass', TestClass)
+print(tolua(table(TestClass._fields)
+	:map(function(x) return {} end) -- table(x, {_env=false}) end)
+))
+print(tolua(table(TestClass._methods)
+	:map(function(x) return {} end) -- table(x, {_env=false}) end)
+))
+print(tolua(table(TestClass._ctors)
+	:map(function(x) return {} end) -- table(x, {_env=false}) end)
+))
+local testObj = TestClass()
+print('testObj', testObj)
+
+local obj = testObj:bar( J:_str("testing") )
+print('foo returns', obj)
